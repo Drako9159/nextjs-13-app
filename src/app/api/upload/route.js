3f79098a8d13a5368fbc1e2526bc47235fefa89c
drako9159 <chochito9159@gmail.com>
2023-05-03 13:44:46 -0600

    add complete views

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,4 +1,26 @@
-export async function GET(request) {
-    return new Response('Uploadinn file!')
-  }
-  
+import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
+import path from "path";
+
+export async function POST(request) {
+  let fileContents = null;
+  let filePath = "";
+  request.formData().then((formData) => {
+    const file = formData.get("file");
+    if(!file){/*
+        return NextResponse.json(
+            JSON.stringify({ error: "No file found in request" }),
+        )*/
+        //return new Response("No file found in request", { status: 400 })
+    }
+
+    filePath = path.join(process.cwd(), "public", file.name);
+    file.arrayBuffer().then((buffer) => {
+      fileContents = Buffer.from(buffer);
+      writeFile(filePath, fileContents);
+      console.log("File written to disk: " + filePath);
+    });
+  });
+
+  return new Response("Uploading file!");
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,44 @@
 "use client";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [file, setFile] = useState(null);
+
+  function handleChange(e){
+    setFile(e.target.files[0]);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!file) return;
+    const formData = new FormData();
+    formData.append("file", file);
+    await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+  }
+
   return (
-    <div>
-      <form>
-        <label>Upload file:</label>
+    <div className="flex h-screen justify-center items-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-950 p-5">
+        <label className="text-4x1 text-center my-4">Upload file:</label>
         <input
+        className="bg-zinc-900 p-2 text-zinc-100 rounded block mb-2"
           type="file"
-          onChange={(e) => {
-            console.log(e.target.files[0]);
-          }}
+          onChange={handleChange}
         ></input>
 
-        <button>Upload</button>
+        <button disable={!file} className="bg-green-500 text-zinc-100 p-2 rounded block w-full disabled:opacity-50" type="submit">Upload</button>
       </form>
+      {
+        file && (
+          <div className="bg-zinc-950 p-5">
+            <p className="text-4x1 text-center my-4">File preview:</p>
+            <img src={URL.createObjectURL(file)} className="max-w-xs" />
+          </div>
+        )
+      }
     </div>
   );
 }
